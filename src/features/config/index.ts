1b9cf0b3d9f9e42c0111e52c54a562cf3e08f89e
FreDCFR8 <frederik.de.causemaeker@gmail.com>
2026-07-07 00:38:01 +0200

    chore: add config feature exports

---
+++ src/features/config/index.ts
@@ -0,0 +1,3 @@
+export type { EnvConfigReadiness, EnvConfigReadinessStatus } from './envConfigReadiness';
+export { getEnvConfigReadiness } from './envConfigReadiness';
+export { EnvConfigStatusCard } from './EnvConfigStatusCard';
