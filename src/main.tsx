60a3a7616f81595db2e1368a438e291cc3e26fe5
FreDCFR8 <frederik.de.causemaeker@gmail.com>
2026-07-22 20:24:40 +0200

    feat: load design system styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { IdentityProvider } from './features/auth/IdentityProvider';
+import './ui/design-system.css';
 import './styles.css';
 
 createRoot(document.querySelector('#root') as Element).render(<IdentityProvider><App /></IdentityProvider>);
