89d80e361686598e67e422b7d0ada2822370fc15
FreDCFR8 <frederik.de.causemaeker@gmail.com>
2026-07-22 19:37:01 +0200

    Export dashboard feature

---
+++ src/features/dashboard/index.ts
@@ -0,0 +1,3 @@
+export { AdminDashboard, ChildDashboard } from './DashboardPanel';
+export { loadAdminDashboard, loadChildDashboard } from './dashboardService';
+export type { DashboardRecentCard, DashboardState, DashboardSummary } from './dashboardTypes';
