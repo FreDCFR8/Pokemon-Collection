e6e24696a21887a1082e5c7fbf35c38ac8c17243
FreDCFR8 <frederik.de.causemaeker@gmail.com>
2026-07-23 11:02:18 +0200

    Load card detail contrast polish

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { IdentityProvider } from './features/auth/IdentityProvider';
 import './ui/design-system.css';
 import './styles.css';
 import './features/cardDetail/cardDetailPolish.css';
+import './features/cardDetail/cardDetailContrastPolish.css';
 import './features/cardDetail/cardDetailSwipeLock';
 
 createRoot(document.querySelector('#root') as Element).render(<IdentityProvider><App /></IdentityProvider>);
