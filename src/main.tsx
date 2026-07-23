56d0d51c83b1d9c54af18e9346ac5274599e4f64
FreDCFR8 <frederik.de.causemaeker@gmail.com>
2026-07-23 11:01:52 +0200

    Enable card detail swipe locking

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import { IdentityProvider } from './features/auth/IdentityProvider';
 import './ui/design-system.css';
 import './styles.css';
 import './features/cardDetail/cardDetailPolish.css';
+import './features/cardDetail/cardDetailSwipeLock';
 
 createRoot(document.querySelector('#root') as Element).render(<IdentityProvider><App /></IdentityProvider>);
